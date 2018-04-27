3f48bc8052ab4f6c7d3aec7c5ed5b6fd4dbbb5ef
dricherson <38729497+dricherson@users.noreply.github.com>
2018-04-27 00:36:28 -0500

    Expanded the body of the page

--- resources/aorta.js
+++ resources/aorta.js
@@ -67,6 +67,7 @@ function HideAll(){
     HideContent('artgrantform');
     HideContent('volunteerform');
     HideContent('Maps');
+    HideContent('Maps');
     /*
     HideContent('mattCoaching');
     HideContent('mattMemo');
@@ -121,5 +122,70 @@ function P7_ExpMenu(){ //v1.1.0.2 by PVII-www.projectseven.com
                 }
             }
         }nn=i+1;
+    }
+}
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime, message) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+  var messageSpan = clock.querySelector('.clockMessage');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    messageSpan.innerHTML = message;
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var schedule = [
+    ['April 20 2018', 'May 01 2018', 'Until A.O.R.T.A. 2018 tickets go on sale at <a href="http://www.burnertickets.com/">BurnerTickets</a>'],
+    ['May 02 2018', 'August 24 2018', 'Until <a href="https://www.facebook.com/events/866728863513899/">A.O.R.T.A. 2018!!!</a>']
+];
+
+function initCountDown(){
+    // iterate over each element in the schedule
+    for(var i=0; i<schedule.length; i++){
+      var startDate = schedule[i][0];
+      var endDate = schedule[i][1];
+      var message = schedule[i][2];
+
+      // put dates in milliseconds for easy comparisons
+      var startMs = Date.parse(startDate);
+      var endMs = Date.parse(endDate);
+      var currentMs = Date.parse(new Date());
+
+      // if current date is between start and end dates, display clock
+      if(endMs > currentMs && currentMs >= startMs ){
+          initializeClock('clockdiv', endDate, message);
+      }
     }
 }
